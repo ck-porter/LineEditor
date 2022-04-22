01103d67f6770965f59e58413352f0b3999e2033
CK Porter <71106072+ck-porter@users.noreply.github.com>
2022-04-22 11:27:32 -0300

    Add files via upload

---
+++ LineEditor/cmake-build-debug/CMakeFiles/assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment1.dir/src/LinkedList.cpp.obj"
+  "CMakeFiles/assignment1.dir/src/main.cpp.obj"
+  "assignment1.exe"
+  "assignment1.exe.manifest"
+  "assignment1.pdb"
+  "libassignment1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
